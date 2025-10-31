606aa4180aa92df81dad748dd19fd22b3fe49fde
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-10-31 14:49:34 -0300

    criação do LedgerSummary

---
+++ LedgerFlow.Tests.Unit/LedgerSummaryUnitTests.cs
@@ -0,0 +1,69 @@
+﻿namespace LedgerFlow.Tests.Unit;
+
+public class LedgerSummaryUnitTests
+{
+    [Fact(DisplayName = "AddTransaction_DeveAdicionarTransacaoCorretamente_QuandoTransacaoForValida")]
+    public void AddTransaction_ShouldAddTransaction_WhenTransactionIsValid()
+    {
+        // Arrange
+        var merchantId = Guid.NewGuid();
+        var summary = new LedgerSummary();
+        var credit = Transaction.CreateCredit(500m, "Venda no cartão");
+        var debit = Transaction.CreateDebit(100m, "Compra de material");
+
+        // Act
+        summary.AddTransaction(credit);
+        summary.AddTransaction(debit);
+
+        // Assert
+        Assert.Equal(2, summary.Transactions.Count);
+        Assert.Equal(500m, summary.TotalCredits);
+        Assert.Equal(100m, summary.TotalDebits);
+        Assert.Equal(400m, summary.FinalBalance);
+    }
+
+    [Theory(DisplayName = "AddTransaction_DeveLancarArgumentNullException_QuandoTransacaoForNula")]
+    [InlineData(true)]
+    public void AddTransaction_ShouldThrowArgumentNullException_WhenTransactionIsNull(bool dummy)
+    {
+        // Arrange
+        var summary = new LedgerSummary();
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => summary.AddTransaction(null!));
+    }
+
+    [Fact(DisplayName = "AddTransactions_DeveAdicionarVariasTransacoesCorretamente_QuandoListaForValida")]
+    public void AddTransactions_ShouldAddMultipleTransactions_WhenListIsValid()
+    {
+        // Arrange
+        var merchantId = Guid.NewGuid();
+        var summary = new LedgerSummary();
+
+        var transactions = new List<Transaction>
+        {
+            Transaction.CreateCredit(100m, "Venda 1"),
+            Transaction.CreateCredit(50m, "Venda 2"),
+            Transaction.CreateDebit(30m, "Compra 1")
+        };
+
+        // Act
+        summary.AddTransactions(transactions);
+
+        // Assert
+        Assert.Equal(3, summary.Transactions.Count);
+        Assert.Equal(150m, summary.TotalCredits);
+        Assert.Equal(30m, summary.TotalDebits);
+        Assert.Equal(120m, summary.FinalBalance);
+    }
+
+    [Fact(DisplayName = "Adicionar transações deve lançar ArgumentNullException quando a lista for nula")]
+    public void AddTransactions_ShouldThrowArgumentNullException_WhenListIsNull()
+    {
+        // Arrange
+        var summary = new LedgerSummary();
+
+        // Act e Assert
+        Assert.Throws<ArgumentNullException>(() => summary.AddTransactions(null!));
+    }
+}
---
+++ LedgerFlow/LedgerSummary.cs
@@ -0,0 +1,34 @@
+﻿namespace LedgerFlow;
+
+public class LedgerSummary
+{
+    public DateTime CreatedAt { get; } = DateTime.UtcNow;
+    public IList<Transaction> Transactions { get; private set; } = [];
+    public decimal TotalCredits { get; private set; }
+    public decimal TotalDebits { get; private set; }
+    public decimal FinalBalance => TotalCredits - TotalDebits;
+
+    public void AddTransaction(Transaction transaction)
+    {
+        if (transaction is null)
+            throw new ArgumentNullException(nameof(transaction));
+
+        Transactions.Add(transaction);
+
+        if (transaction.Type == TransactionType.Credit)
+            TotalCredits += transaction.Value;
+        else
+            TotalDebits += transaction.Value;
+    }
+
+    public void AddTransactions(IEnumerable<Transaction> transactions)
+    {
+        if (transactions is null)
+            throw new ArgumentNullException(nameof(transactions));
+
+        foreach (var t in transactions)
+            AddTransaction(t);
+    }
+
+    public override string ToString() => $"Consolidado para {CreatedAt:yyyy-MM-dd}: +{TotalCredits:C} -{TotalDebits:C} = {FinalBalance:C}";
+}
